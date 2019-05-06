01167ff2f902593f4f50d4c29939211a1075bbb9
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2019-05-06 22:41:41 +0900

    SeederをincrementIDに依存しないようにした

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,92 +20,6 @@ class DatabaseSeeder extends Seeder
 
 }
 
-class ProceduresTableSeeder extends Seeder {
-    public function run()
-    {
-
-        $TABLE_NAME = 'procedures';
-
-        DB::table($TABLE_NAME)->delete();
-        $sql = "ALTER TABLE ".$TABLE_NAME." AUTO_INCREMENT = 1;";
-        DB::unprepared($sql);
-
-        DB::table($TABLE_NAME)->insert([
-            [
-                'training_id' => 1,
-                'description' => 'テスト1',
-                'procedure_data' => '{"lines":
-                    [
-                        { "id": "line_1", "x1": 10, "y1": 10, "x2": 100, "y2": 100}
-                        ,{ "id": "line_2","x1": 10, "y1": 20, "x2": 100, "y2": 200}
-                    ],
-                    "balls":
-                    [
-                        { "id": "ball_1", "x": 30, "y": 30}
-                        ,{ "id": "ball_2", "x": 60, "y": 60}
-                    ]
-                }',
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime()
-            ],
-            [
-                'training_id' => 1,
-                'description' => 'テスト2',
-                'procedure_data' => '{"lines":
-                    [
-                        { "id": "line_1", "x1": 10, "y1": 10, "x2": 100, "y2": 100}
-                        ,{ "id": "line_2","x1": 10, "y1": 20, "x2": 100, "y2": 200}
-                    ],
-                    "balls":
-                    [
-                    ]
-                }',
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime()
-            ],
-        ]);
-    }
-}
-
-class TrainingsTableSeeder extends Seeder {
-
-    public function run()
-    {
-
-        $TABLE_NAME = 'trainings';
-
-        DB::table($TABLE_NAME)->delete();
-        $sql = "ALTER TABLE ".$TABLE_NAME." AUTO_INCREMENT = 1;";
-        DB::unprepared($sql);
-
-        DB::table($TABLE_NAME)->insert([
-            [
-                'title' => 'ストローク５本打ち',
-                'duration_minutes' => 20,
-                'recomended_person_number' => 8,
-                'video_url' => 'https://www.youtube.com/watch?v=3KFp4gqt9wI',
-                'user_id' => 1,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime()
-
-            ],
-            [
-                'title' => 'サイドボレー',
-                'duration_minutes' => 20,
-                'recomended_person_number' => 8,
-                'video_url' => 'https://www.youtube.com/watch?v=kk8fV9h26SA',
-                'user_id' => 2,
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime()
-
-            ]
-        ]);
-
-
-    }
-
-}
-
 class TagsTableSeeder extends Seeder {
 
     public function run()
@@ -118,14 +32,30 @@ class TagsTableSeeder extends Seeder {
         DB::unprepared($sql);
 
         DB::table($TABLE_NAME)->insert([
-            [ 'name' => 'ストローク' ]
-            ,[ 'name' => 'ボレー' ]
-            ,[ 'name' => 'サービス' ]
-            ,[ 'name' => 'レシーブ' ]
-            ,[ 'name' => 'スマッシュ' ]
-            ,[ 'name' => 'フォーメーション' ]
-            ,[ 'name' => 'シングルス' ]
-            ,[ 'name' => 'ウォーミングアップ' ]
+            [ 'name' => 'ストローク',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'ボレー',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'サービス',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'レシーブ',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'スマッシュ',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'フォーメーション',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'シングルス',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
+            ,[ 'name' => 'ウォーミングアップ',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime() ]
         ]);
 
 
@@ -182,15 +112,105 @@ class Tag_TrainingTableSeeder extends Seeder {
         DB::table($TABLE_NAME)->insert([
             [
                 'tag_id' => $stroke_tag_id,
-                'training_id' => $stroke_training_id
+                'training_id' => $stroke_training_id,
             ],
             [
                 'tag_id' => $volley_tag_id,
-                'training_id' => $volley_training_id
+                'training_id' => $volley_training_id,
             ]
         ]);
 
 
     }
 
+}
+
+class TrainingsTableSeeder extends Seeder {
+
+    public function run()
+    {
+
+        $TABLE_NAME = 'trainings';
+
+        DB::table($TABLE_NAME)->delete();
+        $sql = "ALTER TABLE ".$TABLE_NAME." AUTO_INCREMENT = 1;";
+        DB::unprepared($sql);
+
+        $user_test01 = DB::table('users')->where('email', 'test01@test.com')->value('id');
+        $user_test02 = DB::table('users')->where('email', 'test02@test.com')->value('id');
+
+        DB::table($TABLE_NAME)->insert([
+            [
+                'title' => 'ストローク５本打ち',
+                'duration_minutes' => 20,
+                'recomended_person_number' => 8,
+                'video_url' => 'https://www.youtube.com/watch?v=3KFp4gqt9wI',
+                'user_id' => $user_test01,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+
+            ],
+            [
+                'title' => 'サイドボレー',
+                'duration_minutes' => 20,
+                'recomended_person_number' => 8,
+                'video_url' => 'https://www.youtube.com/watch?v=kk8fV9h26SA',
+                'user_id' => $user_test02,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+
+            ]
+        ]);
+
+
+    }
+
+}
+
+class ProceduresTableSeeder extends Seeder {
+    public function run()
+    {
+
+        $TABLE_NAME = 'procedures';
+
+        DB::table($TABLE_NAME)->delete();
+        $sql = "ALTER TABLE ".$TABLE_NAME." AUTO_INCREMENT = 1;";
+        DB::unprepared($sql);
+
+        $stroke_training_id = DB::table('trainings')->where('title', 'ストローク５本打ち')->value('id');
+        DB::table($TABLE_NAME)->insert([
+            [
+                'training_id' => $stroke_training_id,
+                'description' => 'テスト1',
+                'procedure_data' => '{"lines":
+                    [
+                        { "id": "line_1", "x1": 10, "y1": 10, "x2": 100, "y2": 100}
+                        ,{ "id": "line_2","x1": 10, "y1": 20, "x2": 100, "y2": 200}
+                    ],
+                    "balls":
+                    [
+                        { "id": "ball_1", "x": 30, "y": 30}
+                        ,{ "id": "ball_2", "x": 60, "y": 60}
+                    ]
+                }',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'training_id' => $stroke_training_id,
+                'description' => 'テスト2',
+                'procedure_data' => '{"lines":
+                    [
+                        { "id": "line_1", "x1": 10, "y1": 10, "x2": 100, "y2": 100}
+                        ,{ "id": "line_2","x1": 10, "y1": 20, "x2": 100, "y2": 200}
+                    ],
+                    "balls":
+                    [
+                    ]
+                }',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+        ]);
+    }
 }
